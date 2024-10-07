df0bcefaaff472375ccd622410abb54b75165d15
Abovetous <nastena.kozarezova@gmail.com>
2024-10-07 17:17:39 +0300

    Add files via upload

---
+++ modul_1_6..py
@@ -0,0 +1,12 @@
+my_dict = { 'Nastya' : 2002, 'Anna' : 1999, 'Inna' :1988}
+print(my_dict)
+print(my_dict['Anna'])
+my_dict['egor'] =1987
+my_dict['Fedor']=1999
+print(my_dict)
+del my_dict ['egor']
+print(my_dict)
+my_set={1,2,2,3,4,5,7,1,2,2,3, True, 'string'}
+print(my_set)
+my_set.add("egor"), my_set.add('vasya'), my_set.discard('string')
+print(my_set)
